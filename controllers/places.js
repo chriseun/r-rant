048df94bf520e2a01c2274d04cb246f6df4bd940
Chris Eun <eunchristopher@gmail.com>
2023-01-27 00:49:31 -0800

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,12 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
 
 router.get('/', (req, res) => {
   let places = [{
--- index.js
+++ index.js
@@ -8,13 +8,15 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-app.get('/new', (req, res) => {
-  res.render('places/new')
-})
+// app.get('/new', (req, res) => {
+//   res.render('places/new')
+// })
 
 
 
